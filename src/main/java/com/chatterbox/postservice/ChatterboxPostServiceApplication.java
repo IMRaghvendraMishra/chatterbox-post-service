8c2f046cd361aa06891213aa61222a4af244c65c
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-04 19:51:30 +0530

    Add enable kafka annotation

--- src/main/java/com/chatterbox/postservice/ChatterboxPostServiceApplication.java
+++ src/main/java/com/chatterbox/postservice/ChatterboxPostServiceApplication.java
@@ -3,8 +3,10 @@ package com.chatterbox.postservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableKafka
 public class ChatterboxPostServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatterboxPostServiceApplication.class, args);
