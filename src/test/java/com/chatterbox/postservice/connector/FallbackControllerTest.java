978bf3e071a5ec2b0b9d05551226467d682337b5
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-06 20:54:52 +0530

    Implement Fallback controller and unit test

---
+++ src/test/java/com/chatterbox/postservice/connector/FallbackControllerTest.java
@@ -0,0 +1,27 @@
+package com.chatterbox.postservice.connector;
+
+import com.chatterbox.postservice.controller.FallbackController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FallbackController.class)
+public class FallbackControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;  // Mock MVC to send requests and verify responses
+
+    @Test
+    public void handleInvalidPath() throws Exception {
+        // Act
+        ResultActions result = mockMvc.perform(get("/wrong/url"));
+
+        // Assert
+        result.andExpect(status().isNotFound());
+    }
+}
