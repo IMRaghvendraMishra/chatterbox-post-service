b4771eb64301ba4c796d59ca89d01b9734ed4967
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-06 17:26:17 +0530

    Add post validator class

---
+++ src/main/java/com/chatterbox/postservice/validator/PostServiceValidator.java
@@ -0,0 +1,27 @@
+package com.chatterbox.postservice.validator;
+
+import com.chatterbox.postservice.model.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostServiceValidator {
+
+    public void validateNewPost(Post post) {
+
+    }
+
+    public void validateUpdatePost(Post post) {
+
+    }
+
+    public void validateUsername(String username) {
+
+    }
+
+    public void validatePostId(String postId) {
+
+    }
+
+
+
+}
